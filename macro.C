b688b0358317e8957e7fd8adf8ead246f1b8b72f
mzks <mz@stu.kobe-u.ac.jp>
2016-05-08 02:23:01 +0900

    Macro to make ROOT tree

--- macro.C
+++ macro.C
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <TTree.h>
+#include <TString.h>
+#include <TFile.h>
+#include <string>
 
 #ifdef __CINT__
 int macro(){
 #else
 int main(){
 #endif
+	const std::string INPUT_FILE_NAME = "input.dat";
 
-	using namespace std;
-	cout << "Hello,root!!" << endl;
-
-
+	TString ifn = INPUT_FILE_NAME;
+	TTree *tr = new TTree("tr","");
+	tr->ReadFile(ifn,"row1/I:row2/D:row3/D:row4/D");
+	TString ofn = "data.root";
+	TFile *fout = new TFile(ofn,"recreate");
+	tr->Write();
+	fout->Close();
 	return 0;
 }
