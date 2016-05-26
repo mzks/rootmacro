6fb46f89d4062852c54dd155c6f84411505b63ac
mzks <mz@stu.kobe-u.ac.jp>
2016-05-26 16:20:43 +0900

    Code shaping data(ex.MCA data)

--- cut.sh
+++ cut.sh
@@ -1,5 +1,17 @@
-# /bin/sh
-file=$1
-head -8208 $file | tail -8192  > $file.dat
-echo "input file:" $file
-echo "output file:" $file.dat
+#!/bin/sh
+
+#cut.sh #################################
+# Author Mizukoshi Keita
+# 2016 May 26
+# Usage:
+# ./cut.sh filename 10 20
+# 10 is number of lines cutting from Head
+# 20 is number of lines cutting from Tail
+#########################################
+
+FILE=$1
+NLINE=(`wc -l $1`)
+HEAD=`expr $NLINE - $2 - $3`
+TAIL=`expr $NLINE - $3`
+head -$TAIL $FILE|tail -$HEAD
+
